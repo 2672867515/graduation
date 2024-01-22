2ef982eea3457757e556549d05132431ada05639
吴鹏辉 <2672867515@qq.com>
2024-01-22 16:20:18 +0800

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import './index.scss'
-import home from '../../img/home.jpg';
+import banner1 from '../../img/banner1.jpg';
+import banner2 from '../../img/banner2.png';
+import banner3 from '../../img/banner3.png';
 const Page1=(props)=> {
   let   history = useHistory() //将useHistory()钩子赋值给history方便使用
-  const [url,setUrl]=useState(home)
+  const bannerArr=[banner1,banner2,banner3]
+  const [url,setUrl]=useState(bannerArr[0])
+
+  useEffect(()=>{
+    let i=0
+    setInterval(()=>{
+      i++
+      setUrl(bannerArr[i])
+      if(i===3){
+        setUrl(bannerArr[0])
+        i=0
+      }
+    },3000)
+  },[])
 
   const login=(id:number)=>{
     history.push(`/detail/${id}`)
@@ -12,7 +27,7 @@ const Page1=(props)=> {
   return (
     <div className='mainbox'>
       <img className="bg" src={url} alt='bg' />
-      <div className="text">
+      <div className="bg-text">
       基于three.js的3D选房平台
       </div>
       <div className="content">
